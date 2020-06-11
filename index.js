990fa02e0099faec84d6eee136c9398dc153401c
Slikon <slava.kulik27@gmail.com>
2020-06-11 20:01:42 +0300

    env created; gitignore fixed accordingly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegraf = require("telegraf");
 const axios = require("axios");
 
-const bot = new Telegraf("");
+const bot = new Telegraf(process.env.bot_token)
 
 bot.start((ctx) => {
   ctx.reply("Bot started!\nType /help to see what I can!");
