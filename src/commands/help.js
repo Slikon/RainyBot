3a940353d2859606b51ade77a3eba6036e6d7759
Slikon <slava.kulik27@gmail.com>
2020-09-28 23:31:21 +0300

    minor fix

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 const helpCommand = (ctx) => {
-  ctx.reply(`Here is a list of commands:
-/location - enter your current location 
-/check - check your last saved location`);
+  ctx.reply(
+    `Here is a list of commands:\n/location - enter your current location\n/check - check your last saved location`
+  );
 };
 
 module.exports = { helpCommand };
