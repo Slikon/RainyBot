219f3aed71292addc850674dc070bbabafe2ec50
Slikon <slava.kulik27@gmail.com>
2020-09-29 03:10:43 +0300

    fix: weather-provider changed; minor fixes in getWeather and app.js

--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 //function returns a weather report for user's location coords
 const getWeather = async (user) => {
   let url = encodeURI(
-    `http://www.7timer.info/bin/civillight.php?lon=${user.longtitudeLocation}&lat=${user.latitudeLocation}&ac=0&unit=metric&output=json&tzshift=0`
+    `http://api.openweathermap.org/data/2.5/weather?lat=${user.latitudeLocation}&lon=${user.longtitudeLocation}&appid=${process.env.weather_key}`
   );
   try {
     let res = await axios.get(url);
-    let fullWeather = await res.data.dataseries[0];
-    let usrWeather = await fullWeather.weather;
+    let fullWeather = await res.data.weather[0];
+    let usrWeather = await fullWeather.description;
 
     return usrWeather;
   } catch (error) {
