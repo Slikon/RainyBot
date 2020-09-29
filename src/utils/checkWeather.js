61eb551793eb8f85527ff9a595a6557035a394c3
Slikon <slava.kulik27@gmail.com>
2020-09-29 13:26:03 +0300

    minor fix: 'drizzl' added to triggers array in checkWeather();

--- src/utils/checkWeather.js
+++ src/utils/checkWeather.js
@@ -6,7 +6,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // function checks if weather today is rainy/snowy and reports directly to user if true
 const checkWeather = async () => {
-  let triggers = ['rain', 'snow', 'shower'];
+  let triggers = ['rain', 'snow', 'shower', 'drizzl'];
 
   const users = await User.find({});
   users.forEach(async (user) => {
