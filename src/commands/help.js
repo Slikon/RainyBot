ea2b918d2376f827bf7d47acdd44f77dbdfae931
Viacheslav Kulyk <vkulyk@zoolatech.com>
2020-11-28 20:37:23 +0200

    command 'help' minor fix

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 const helpCommand = (ctx) => {
   ctx.reply(
-    `Hi! This bot is created to warn you in advance, if it would be rainy/snowy in your city today. You won't forget to grab an umbrella with you in the morning anymore :D.\nEnter your location and that's all! If precipitation is expected today, I would notice you automatically at 03:00 (UTC). All notifications are sent automatically and you don't need to configure me. Enjoy!
-  \nHere is a list of commands:\n/location - save your current location so I would know where you are :)\n/check - check your last saved location and recieve your weather forecast for today!`
+    `Hi! This bot is created to warn you about possible weather precipitations in your region. I usually forget to check out the weather and grab an umbrella, that's why managed to create this bot.\n\nEnter your location and that's all! If precipitations are expected today, I would notice you automatically at 03:00 (UTC). All notifications are sent automatically and you don't need to configure me. Enjoy!
+  \n\nHere is a list of commands:\n/location - save your current location so I would know where you are :)\n/check - check your last saved location and recieve your weather forecast for today!`
   );
 };
 
